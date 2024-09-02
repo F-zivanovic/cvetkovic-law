c6e1c85f91511dfb54090b4486fe21b9b03f70d1
F-zivanovic <filip.zivanovic1999@gmail.com>
2024-09-02 10:09:23 +0200

    ajmmooo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,14 @@
 const hamburger = document.getElementById("hamburger");
 const navList = document.getElementById("nav__list");
+const navLinks = document.querySelectorAll(".list__link");
 
 hamburger.addEventListener('click', () =>{
     navList.classList.toggle('open');
+});
+
+
+navLinks.forEach(link => {
+    link.addEventListener('click', ()=>{
+        navList.classList.remove('open');
+    })
 });
