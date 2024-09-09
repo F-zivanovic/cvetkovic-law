efae557203887f9ee9962cbaefb912228fa6d804
F-zivanovic <filip.zivanovic1999@gmail.com>
2024-09-09 13:05:26 +0200

    nav gotov

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,13 +2,24 @@ const hamburger = document.getElementById("hamburger");
 const navList = document.getElementById("nav__list");
 const navLinks = document.querySelectorAll(".list__link");
 
+
 hamburger.addEventListener('click', () =>{
     navList.classList.toggle('open');
 });
 
 
-navLinks.forEach(link => {
-    link.addEventListener('click', ()=>{
-        navList.classList.remove('open');
-    })
+
+// Open/close desktop services dropdown
+const desktopDropdown = document.querySelector('.dropdown');
+const navDropdown = document.querySelector('.dropdown > .nav__dropdown');
+
+desktopDropdown.addEventListener('click', ()=>{
+    navDropdown.classList.toggle('open__dropdown');
+})
+
+
+// Close Mobile menu
+const closeIcon = document.getElementById('close');
+closeIcon.addEventListener('click', ()=>{
+    navList.classList.remove('open');
 });
